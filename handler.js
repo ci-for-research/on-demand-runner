c7148d1b592b43154118e2f17abf563010e416b0
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2020-06-19 15:39:05 +0200

    Disable check

--- handler.js
+++ handler.js
@@ -3,26 +3,27 @@
 module.exports.check_run = async event => {
   const body = JSON.parse(event.body)
   const action = body.action
+  console.log(JSON.stringify(action))
 
-  const block_external_prs = true
+  // const block_external_prs = true
   
-  const is_public_repo = !action.repository.private
-  // TODO https://developer.github.com/v3/actions/workflow-runs/#get-a-workflow-run
-  // fetch run info from `action.check_run.url` and check event type
-  // const run_info = await fetch(action.check_run.url, <access token>)
-  // const is_push = run_info.event !== 'push'
-  const is_push = true
-  //TODO
-  // check whether pr comes from fork
-  // const is_internal_pr = run_info.repository.full_name === run_info.head_repository.full_name
-  const is_internal_pr = true;
+  // const is_private_repo = action.repository.private
+  // // TODO https://developer.github.com/v3/actions/workflow-runs/#get-a-workflow-run
+  // // fetch run info from `action.check_run.url` and check event type
+  // // const run_info = await fetch(action.check_run.url, <access token>)
+  // // const is_push = run_info.event !== 'push'
+  // const is_push = true
+  // //TODO
+  // // check whether pr comes from fork
+  // // const is_internal_pr = run_info.repository.full_name === run_info.head_repository.full_name
+  // const is_internal_pr = true;
 
-  let is_external_pr = is_public_repo && !is_push && !is_internal_pr
+  // let is_external_pr = !is_private_repo && !is_push && !is_internal_pr
 
-  if (block_external_prs && is_external_pr) {
-    console.log('External PR detected please cancel job');
-    // TODO also do action
-  }
+  // if (block_external_prs && is_external_pr) {
+  //   console.log('External PR detected please cancel job at ' + action.check_run.html_url);
+  //   // TODO also do action
+  // }
 
   if (action === 'created') {
     console.log('Spin up runner now')
